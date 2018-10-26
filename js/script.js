52825b7e12b7660767fed89e703352c91601cb2a
StudentEvgeshka <43698839+StudentEvgeshka@users.noreply.github.com>
2018-10-26 23:42:53 +0300

    замена

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-    $(document).ready(function() {
-     
-      $("#owl-example").owlCarousel();
-     
-    });
+$(document).ready(function(){
+	
+	// Находим блок карусели
+	var carousel = $("#carousel");
+
+	// Запускаем плагин карусели
+	carousel.owlCarousel();
+});
+// Назад
+// При клике на "Назад"
+$('#js-prev').click(function () {
+
+	// Запускаем перемотку влево
+	carousel.trigger('owl.prev');
+
+	return false;
+});
+
+// Вперед
+// При клике на "Вперед"
+$('#js-next').click(function () {
+
+	// Запускаем перемотку вправо
+	carousel.trigger('owl.next');
+
+	return false;
+});
